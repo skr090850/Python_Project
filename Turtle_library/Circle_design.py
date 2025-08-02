4d42dc6000b63368d19e1de5c5575e55a05b141a
skr090850 <skr090850@gmail.com>
2025-08-02 13:40:31 +0530

    Insert Pause function

--- Turtle_library/Circle_design.py
+++ Turtle_library/Circle_design.py
@@ -1,4 +1,5 @@
 from turtle import*
+import turtle
 t=Turtle()
 title("Circle")
 speed(5)
@@ -6,3 +7,5 @@ t.hideturtle()
 pensize(2)
 for i in range(100,0,-10):
     circle(i,360)
+    
+turtle.exitonclick()
